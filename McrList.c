b340a86f59975f0ea25e3698fcac719b55784ddf
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-01-15 15:06:25 +0200

    linked list for phase zero

--- McrList.c
+++ McrList.c
@@ -1,6 +1,19 @@
+#include "McrList.h"
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 struct nodeMcr{
     char *name;/* name of the node */
     int start;/* start index of the macro */
     int end; /* length of the macro */
     McrList next;  /* Next link */
 };
+McrList NewMcr(char *name, int start) {
+    McrList new_mcr = (McrList) calloc(1, sizeof(struct nodeMcr));
+    new_mcr->name = (char *) calloc(1, (strlen(name) + 1) * sizeof(char));
+    strcpy(new_mcr->name, name);
+    new_mcr->start = start;
+    new_mcr->end = 0;
+    new_mcr->next = NULL;
+    return new_mcr;
+}
---
+++ McrList.h
@@ -0,0 +1,9 @@
+//
+// Created by טל פלקסין on 15/01/2024.
+//
+
+#ifndef M1_MCRLIST_H
+#define M1_MCRLIST_H
+/*A pointer to a linked list*/
+typedef struct nodeMcr *McrList;
+#endif //M1_MCRLIST_H
