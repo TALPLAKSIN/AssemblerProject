6e2e79651a8d45b7aa5a36a786a65324781705bb
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-01-20 12:44:28 +0200

    open_file

--- FileMethod.c
+++ FileMethod.c
@@ -6,3 +6,13 @@ void fullNameToFile(char *fileN, char *extension, char *fullFileName) {
     fullFileName[strlen(fileN)] = '.';
     strcat(fullFileName + (strlen(fileN)) + 1, extension);/*Add a extension to the file*/
 }
+
+
+FILE *open_file(char *file_name, char *fileExtension, char *mode) {
+    char fileNameWithExtension[MAX_FILE_NAME] = {0};
+    FILE *file_To_Open;
+    fullNameToFile(file_name, fileExtension, fileNameWithExtension);
+    /*Opening the file with matching extension according to received mode*/
+    file_To_Open = fopen(fileNameWithExtension, mode);
+    return file_To_Open;
+}
--- FileMethod.h
+++ FileMethod.h
@@ -6,4 +6,5 @@
 #include <stdio.h>
 #define MAX_LINE_LENGTH 80
 void fullNameToFile(char *fileN, char *extension, char *fullFileName) ;
+FILE *open_file(char *file_name, char *fileExtension, char *mode) ;
 #endif
