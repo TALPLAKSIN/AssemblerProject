8e5ff587369ce21ffd73188717ff443906e208b1
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-02-23 17:11:32 +0200

    TagList

--- TagList.c
+++ TagList.c
@@ -46,3 +46,89 @@ TagList SearchTag(TagList tag, char *name) {
         return temp;
     return SearchTag(tag->next, name);
 }
+
+void allUsed(TagList tag, int phase) {
+    int flag = TRUE;
+    while (tag != NULL) {
+        if (tag->used != EXIST) {/*see unused link*/
+            if (flag) {/*the flag marks the first time we see unused link*/
+                if (phase == 0)/*message belonging to a reader layout at  phase zero */
+                    printf("Warning occurred at the process, macros we didn't use: '%s'", tag->name);
+                else/*message belonging to the phase one*/
+                    printf("Warning occurred at the process, symbols we didn't use: '%s'", tag->name);
+                flag = FALSE;
+            } else
+                printf(",'%s'", tag->name);/*Printing another unused name*/
+        }
+        tag = tag->next;/* move to check the next node*/
+    }
+    if (!flag)
+        printf("\n");
+}
+
+
+void moveHead(TagList *head) {
+    TagList temp = *head;
+    *head = (*head)->next;
+    freeLinked(temp);
+}
+
+char *getName(TagList tag) {
+    return tag->name;
+}
+
+
+/*int getAddress(TagList tag) {
+    return tag->address;
+}*/
+
+int getValueOrSize(TagList tag) {
+    return tag->valueOrSize;
+}
+
+Type getType(TagList tag) {
+    return tag->type;
+}
+
+/*TagList getNext(TagList tag) {
+    return tag->next;
+}*/
+
+void setUsed(TagList tag, int used) {
+    tag->used = used;
+}
+
+void setValueOrSize(TagList tag, int valueOrSize) {
+    tag->valueOrSize = valueOrSize;
+}
+
+void setAddress(TagList tag, int address) {
+    tag->address = address;
+}
+
+void setType(TagList tag, Type type) {
+    tag->type = type;
+}
+
+void freeLinked(TagList tag) {
+    free(tag->name);
+    free(tag);
+}
+
+void freelist(TagList head) {
+    TagList prev, current = head;
+    while (current != NULL) {
+        prev = current;
+        current = current->next;
+        freeLinked(prev);
+    }
+}
+
+void update_IC_symbols(TagList *linked, int IC) {
+    TagList temp = *linked;
+    while (temp != NULL) {
+        if (temp->type == DATA)/* if it's a data instruction */
+            temp->address += IC;/* updates the IC counter */
+        temp = temp->next;
+    }
+}
--- TagList.h
+++ TagList.h
@@ -4,6 +4,11 @@
 
 /*A pointer to a linked list*/
 typedef struct nodeTag *TagList;
+#define TRUE 1
+#define FALSE 0
+#define FLAGS 5
+#define NON 9999/*no exist*/
+#define EXIST 1
 /*symbol types option*/
 typedef enum Type {
     MDEFINE,
@@ -14,5 +19,20 @@ typedef enum Type {
 } Type;
 TagList NewTag(char *name, int address) ;
 void InsertTagToList(TagList *head, TagList new_tag) ;
+void allUsed(TagList tag, int phase);
 TagList SearchTag(TagList tag, char *name) ;
+void moveHead(TagList *head);
+char* getName(TagList tag) ;
+/*int getAddress(TagList tag) ;*/
+int getValueOrSize(TagList tag);
+Type getType(TagList tag) ;
+/*TagList getNext(TagList tag) ;*/
+void setUsed(TagList tag, int used) ;
+void setAddress(TagList tag, int address) ;
+void setType(TagList tag, Type type) ;
+void setValueOrSize(TagList tag, int valueOrSize) ;
+void freeLinked(TagList tag) ;
+void freelist(TagList head) ;
+void update_IC_symbols(TagList *linked, int IC) ;
+
 #endif //M1_TAGLIST_H
