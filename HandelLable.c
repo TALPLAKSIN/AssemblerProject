43e4c2435b00620e0ad9965c357c69e06ad40b46
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-03-12 14:57:15 +0200

    HandelLable

--- HandelLable.c
+++ HandelLable.c
@@ -13,5 +13,6 @@ int HandelTagName(char *name) {
 int handel_label(TagList *HeadTagList, char *labelName, int IC, int countLine, TagList *TagTemp) {
     char restOfLine[MAX_LINE_LENGTH]={0};
     labelName[strlen(labelName) - 1] = '\0'; /*we are cutting '\n' from it*/
+    copy(restOfLine, labelName);
 
 }
