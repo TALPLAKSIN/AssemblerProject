f9631b3d45676d428239938760b71b991e5264e8
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-03-18 13:59:17 +0200

    main

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 int main(int argc, char **argv) {
     /* each cell of the markValues array represent a flag (5 cell - ICcounter, DCcounter, EX, EN, ERROR ) */
     int markValues[FLAGS] = {NON, NON, NON, NON, NON};
+    TagList HeadTagList = NULL;
+    int i;
     if (argc < 2) {
         printf("Error - Invalid number of arguments\n");
         return 0;
