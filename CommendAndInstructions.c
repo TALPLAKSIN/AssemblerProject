d409ac5d97ae8398a550277fde4d69dca0afa725
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-03-19 18:01:20 +0200

    new

--- CommendAndInstructions.c
+++ CommendAndInstructions.c
@@ -1,2 +1,4 @@
 
 #include "CommendAndInstructions.h"
+int instructionsFormat(TagList *HeadTagList, char *curWord, int *DC, FILE *checksEntry, int countLine, TagList *TagTemp,
+                       int *markValues, long prevLocation, FILE *inputFile) {
