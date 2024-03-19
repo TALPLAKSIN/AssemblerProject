c58eda413af2cbe5ada4dab6a4904af5710968ea
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-03-19 18:06:34 +0200

    Commend

--- CommendAndInstructions.c
+++ CommendAndInstructions.c
@@ -2,3 +2,7 @@
 #include "CommendAndInstructions.h"
 int instructionsFormat(TagList *HeadTagList, char *curWord, int *DC, FILE *checksEntry, int countLine, TagList *TagTemp,
                        int *markValues, long prevLocation, FILE *inputFile) {
+    if (!strcmp(".define", curWord))
+        return;
+
+}
