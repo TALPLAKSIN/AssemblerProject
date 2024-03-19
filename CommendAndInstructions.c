a8e5352d18e736c9bf9cc2581defbb9ed9030b02
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-03-19 18:14:55 +0200

    Commend

--- CommendAndInstructions.c
+++ CommendAndInstructions.c
@@ -3,6 +3,7 @@
 int instructionsFormat(TagList *HeadTagList, char *curWord, int *DC, FILE *checksEntry, int countLine, TagList *TagTemp,
                        int *markValues, long prevLocation, FILE *inputFile) {
     if (!strcmp(".define", curWord))
-        return;
+        return checkDefineFormat(HeadTagList, strtok(NULL, ""), DC, countLine, TagTemp);
 
 }
+int checkDefineFormat(TagList *HeadTagList, char *line, int *DC, int countLine, TagList *TagTemp) {}
--- CommendAndInstructions.h
+++ CommendAndInstructions.h
@@ -17,4 +17,5 @@ enum flagsNames {
 int
 instructionsFormat(TagList *HeadTagList, char *curWord, int *DC, FILE *checksEntry, int countLine, TagList *TagTemp,
                    int *markValues, long prevLocation, FILE *inputFile);
+int checkDefineFormat(TagList *HeadTagList, char *line, int *DC, int countLine, TagList *TagTemp);
 #endif //M1_COMMENDANDINSTRUCTIONS_H
