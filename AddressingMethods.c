a824dd93adb92914c74c38e460aa112680916625
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-03-20 18:27:14 +0200

    AddressingMethods

--- AddressingMethods.c
+++ AddressingMethods.c
@@ -3,6 +3,7 @@ int instructionValidName(char *instruction){
     int i;
     for (i = 0; i < MAX_NUM_INSTRUCTION; i++) {
         if (strcmp(instruction, instructionNames[i].name) == 0)
+            return TRUE;
 
     }
 
