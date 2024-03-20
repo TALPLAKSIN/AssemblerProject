69db938c87296ed8af23e760d1604875a313732e
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-03-20 18:25:55 +0200

    AddressingMethods

--- AddressingMethods.c
+++ AddressingMethods.c
@@ -1,6 +1,8 @@
 #include "AddressingMethods.h"
 int instructionValidName(char *instruction){
     int i;
-    for (i = 0; i < MAX_NUM_INSTRUCTION; i++) {}
+    for (i = 0; i < MAX_NUM_INSTRUCTION; i++) {
+
+    }
 
 }
--- AddressingMethods.h
+++ AddressingMethods.h
@@ -1,9 +1,11 @@
 //
 // Created by טל פלקסין on 20/03/2024.
 //
-
+#include "CommendAndInstructions.h"
 #ifndef M1_ADDRESSINGMETHODS_H
 #define M1_ADDRESSINGMETHODS_H
+
 #define MAX_NUM_INSTRUCTION 4
+
 int instructionValidName(char *instruction);
 #endif //M1_ADDRESSINGMETHODS_H
