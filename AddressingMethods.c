4312bf5a2c762023f2dfa75598a4c7786f365e13
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-03-20 18:22:20 +0200

    AddressingMethods

---
+++ AddressingMethods.c
@@ -0,0 +1,5 @@
+//
+// Created by טל פלקסין on 20/03/2024.
+//
+
+#include "AddressingMethods.h"
---
+++ AddressingMethods.h
@@ -0,0 +1,8 @@
+//
+// Created by טל פלקסין on 20/03/2024.
+//
+
+#ifndef M1_ADDRESSINGMETHODS_H
+#define M1_ADDRESSINGMETHODS_H
+
+#endif //M1_ADDRESSINGMETHODS_H
