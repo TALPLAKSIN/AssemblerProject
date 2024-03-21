b7d3336a2bd15a7e64d44bbf9ceee798e22728fd
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-03-21 16:51:47 +0200

    AddressingMethods

--- AddressingMethods.h
+++ AddressingMethods.h
@@ -8,4 +8,6 @@
 #define MAX_NUM_INSTRUCTION 4
 
 int instructionValidName(char *instruction);
+int validateCommand(char *command, int *IC, FILE *validLabelFile, int countLine, long previousLocation,
+                    FILE *inputFile, TagList *HeadTagList);
 #endif //M1_ADDRESSINGMETHODS_H
