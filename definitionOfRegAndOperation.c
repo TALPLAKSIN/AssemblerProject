c3972e46016313c84399373e20819366609dd6c4
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-03-21 17:28:07 +0200

    definitionOfRegAndOperation

--- definitionOfRegAndOperation.c
+++ definitionOfRegAndOperation.c
@@ -22,22 +22,24 @@ static const struct operationConsts {
     int endOp;
 /*An array representing all legal operations, their number,
  amount of parameters, source address, destination address, for each method*/
-}operation[] = {{"mov", 0,  2, ALL_OPERATION,     WITHOUT_FIRST_OPERATION},
-                {"cmp", 1,  2, ALL_OPERATION,     ALL_OPERATION},
-                {"add", 2,  2, ALL_OPERATION,     WITHOUT_FIRST_OPERATION},
-                {"sub", 3,  2, ALL_OPERATION,     WITHOUT_FIRST_OPERATION},
-                {"not", 4,  1, 0,                 WITHOUT_FIRST_OPERATION},
-                {"clr", 5,  1, 0,                 WITHOUT_FIRST_OPERATION},
-                {"lea", 6,  2, ONE_TWO_OPERATION, WITHOUT_FIRST_OPERATION},
-                {"inc", 7,  1, 0,                 WITHOUT_FIRST_OPERATION},
-                {"dec", 8,  1, 0,                 WITHOUT_FIRST_OPERATION},
-                {"jmp", 9,  1, 0,                 WITHOUT_FIRST_OPERATION},
-                {"bne", 10, 1, 0,                 WITHOUT_FIRST_OPERATION},
-                {"red", 11, 1, 0,                 WITHOUT_FIRST_OPERATION},
-                {"prn", 12, 1, 0,                 ALL_OPERATION},
-                {"jsr", 13, 1, 0,                 WITHOUT_FIRST_OPERATION},
-                {"rts", 14, 0, 0, 0},
-                {"hlt", 15, 0, 0, 0}};
+} operation[] = {{"mov", 0,  2, ALL_OPERATION,     WITHOUT_FIRST_OPERATION},
+                 {"cmp", 1,  2, ALL_OPERATION,     ALL_OPERATION},
+                 {"add", 2,  2, ALL_OPERATION,     WITHOUT_FIRST_OPERATION},
+                 {"sub", 3,  2, ALL_OPERATION,     WITHOUT_FIRST_OPERATION},
+                 {"not", 4,  1, 0,                 WITHOUT_FIRST_OPERATION},
+                 {"clr", 5,  1, 0,                 WITHOUT_FIRST_OPERATION},
+                 {"lea", 6,  2, ONE_TWO_OPERATION, WITHOUT_FIRST_OPERATION},
+                 {"inc", 7,  1, 0,                 WITHOUT_FIRST_OPERATION},
+                 {"dec", 8,  1, 0,                 WITHOUT_FIRST_OPERATION},
+                 {"jmp", 9,  1, 0,                 WITHOUT_FIRST_OPERATION},
+                 {"bne", 10, 1, 0,                 WITHOUT_FIRST_OPERATION},
+                 {"red", 11, 1, 0,                 WITHOUT_FIRST_OPERATION},
+                 {"prn", 12, 1, 0,                 ALL_OPERATION},
+                 {"jsr", 13, 1, 0,                 WITHOUT_FIRST_OPERATION},
+                 {"rts", 14, 0, 0, 0},
+                 {"hlt", 15, 0, 0, 0}};
 
+
+int check_reg_name(char *validName) {}
 
 
