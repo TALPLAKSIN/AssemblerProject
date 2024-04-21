cf9d3d883c0acc3e2e22ba08e0b2a9d80c4ceee2
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-04-21 18:03:33 +0300

    AddresingMethods

--- definitionOfRegAndOperation.c
+++ definitionOfRegAndOperation.c
@@ -1,83 +1,85 @@
 #include "definitionOfRegAndOperation.h"
 #include "TagList.h"
 #include <string.h>
-static const struct registerConsts {
+static const struct regConsts {
     const char *name;
-    int value;
-    /*An array representing all valid registers*/
-}registers[] = {{"r0", 0},
-                {"r1", 1},
-                {"r2", 2},
-                {"r3", 3},
-                {"r4", 4},
-                {"r5", 5},
-                {"r6", 6},
-                {"r7", 7}
+    int num;
+/*all valid registers*/
+} reg[] = {{"r0", 0},
+           {"r1", 1},
+           {"r2", 2},
+           {"r3", 3},
+           {"r4", 4},
+           {"r5", 5},
+           {"r6", 6},
+           {"r7", 7}
 };
+/* Each operation is defined with its number, opcode, number of operands, and source, destination address*/
 static const struct operationConsts {
-    const char *command;
+    const char *sentence;
     int opcode;
-    int numOfOperation;
-    int startOp;
-    int endOp;
-/*An array representing all legal operations, their number,
- amount of parameters, source address, destination address, for each method*/
-} operation[] = {{"mov", 0,  2, ALL_OPERATION,     WITHOUT_FIRST_OPERATION},
-                 {"cmp", 1,  2, ALL_OPERATION,     ALL_OPERATION},
-                 {"add", 2,  2, ALL_OPERATION,     WITHOUT_FIRST_OPERATION},
-                 {"sub", 3,  2, ALL_OPERATION,     WITHOUT_FIRST_OPERATION},
-                 {"not", 4,  1, 0,                 WITHOUT_FIRST_OPERATION},
-                 {"clr", 5,  1, 0,                 WITHOUT_FIRST_OPERATION},
-                 {"lea", 6,  2, ONE_TWO_OPERATION, WITHOUT_FIRST_OPERATION},
-                 {"inc", 7,  1, 0,                 WITHOUT_FIRST_OPERATION},
-                 {"dec", 8,  1, 0,                 WITHOUT_FIRST_OPERATION},
-                 {"jmp", 9,  1, 0,                 WITHOUT_FIRST_OPERATION},
-                 {"bne", 10, 1, 0,                 WITHOUT_FIRST_OPERATION},
-                 {"red", 11, 1, 0,                 WITHOUT_FIRST_OPERATION},
-                 {"prn", 12, 1, 0,                 ALL_OPERATION},
-                 {"jsr", 13, 1, 0,                 WITHOUT_FIRST_OPERATION},
-                 {"rts", 14, 0, 0, 0},
-                 {"hlt", 15, 0, 0, 0}};
+    int amount_operation;
+    int start_opcode;
+    int end_opcode;
+} the_operations[] = {{"mov", 0,  2, ALL_OPERATION,     WITHOUT_FIRST_OPERATION},
+                      {"cmp", 1,  2, ALL_OPERATION,     ALL_OPERATION},
+                      {"add", 2,  2, ALL_OPERATION,     WITHOUT_FIRST_OPERATION},
+                      {"sub", 3,  2, ALL_OPERATION,     WITHOUT_FIRST_OPERATION},
+                      {"not", 4,  1, 0,                 WITHOUT_FIRST_OPERATION},
+                      {"clr", 5,  1, 0,                 WITHOUT_FIRST_OPERATION},
+                      {"lea", 6,  2, ONE_TWO_OPERATION, WITHOUT_FIRST_OPERATION},
+                      {"inc", 7,  1, 0,                 WITHOUT_FIRST_OPERATION},
+                      {"dec", 8,  1, 0,                 WITHOUT_FIRST_OPERATION},
+                      {"jmp", 9,  1, 0,                 ONE_THREE_OPERATION},
+                      {"bne", 10, 1, 0,                 ONE_THREE_OPERATION},
+                      {"red", 11, 1, 0,                 WITHOUT_FIRST_OPERATION},
+                      {"prn", 12, 1, 0,                 ALL_OPERATION},
+                      {"jsr", 13, 1, 0,                 ONE_THREE_OPERATION},
+                      {"rts", 14, 0, 0, 0},
+                      {"hlt", 15, 0, 0, 0}};
 
 
-int check_reg_name(char *validName) {
+int check_reg_name(char *word) {
     int i;
     for (i = 0; i <= MAX_NUM_REGISTERS; i++) {
-        if (strcmp(validName, registers[i].name) == 0)
+        if (strcmp(word, reg[i].name) == 0)
             return TRUE;
     }
     return FALSE;
 }
-int get_reg_number(char *validName) {
+
+int get_reg_number(char *word) {
     int i;
     for (i = 0; i <= MAX_NUM_REGISTERS; i++) {
-        if (strcmp(validName, registers[i].name) == 0)
-            return registers[i].value;
+        if (strcmp(word, reg[i].name) == 0)
+            return reg[i].num;
     }
     return FALSE;
 }
-int check_op_name(char *command) {
+
+int check_op_name(char *word) {
     int i;
     for (i = 0; i < MAX_OPERATIONS; i++) {
-        if (!strcmp(command, operation[i].command))
+        if (!strcmp(word, the_operations[i].sentence))
             return TRUE;
     }
     return FALSE;
 }
-int get_opcode(char *command){
+
+int get_opcode(char *word) {
     int i;
     for (i = 0; i < MAX_OPERATIONS; i++) {
-        if (!strcmp(command, operation[i].command))
-            return operation[i].opcode;
+        if (!strcmp(word, the_operations[i].sentence))
+            return the_operations[i].opcode;
     }
     return NON;
 }
 
-int get_num_of_operation(char *command) {
+int get_num_of_operation(char *word) {
     int i;
     for (i = 0; i < MAX_OPERATIONS; i++) {
-        if (strcmp(command, operation[i].command) == 0)
-            return operation[i].numOfOperation;
+        if (strcmp(word, the_operations[i].sentence) == 0)
+            return the_operations[i].amount_operation;
     }
     return NON;
 }
--- definitionOfRegAndOperation.h
+++ definitionOfRegAndOperation.h
@@ -1,17 +1,57 @@
-//
-// Created by טל פלקסין on 18/03/2024.
-//
 
-#ifndef M1_DEFINITIONOFREGANDOPERATION_H
-#define M1_DEFINITIONOFREGANDOPERATION_H
+#ifndef MMN14_22_03_24_DEFINITIONOFREGANDOPERATION_H
+#define MMN14_22_03_24_DEFINITIONOFREGANDOPERATION_H
+
+
+
+
 #define MAX_OPERATIONS 16
 #define ONE_TWO_OPERATION 2
+#define ONE_THREE_OPERATION 2
 #define WITHOUT_FIRST_OPERATION 3
 #define ALL_OPERATION 4
 #define MAX_NUM_REGISTERS 7
-int check_reg_name(char *validName);
-int get_reg_number(char *validName);
-int check_op_name(char *command);
-int get_opcode(char *command);
-int get_num_of_operation(char *command);
-#endif //M1_DEFINITIONOFREGANDOPERATION_H
+
+/*
+   This function checks if register name is valid for example (r0,r1..r7)
+   parameters:
+                @param word -the pointer to the register name
+    return:
+                @return TRUE if it valid register,otherwise FALSE
+ */
+int check_reg_name(char *word);
+/*
+   This function return  register number if it valid for example (r0,r1..r7)
+   parameters:
+                @param word -the pointer to the register
+    return:
+                @return register number,otherwise FALSE
+ */
+int get_reg_number(char *word);
+/*
+   This function checks if operation name is valid for example (mov,cmp..)
+   parameters:
+                @param word -the pointer to the command  name
+    return:
+                @return TRUE if its valid operation,otherwise FALSE
+ */
+int check_op_name(char *word);
+/*
+   This function return the opcode operation number if it valid operation for example (mov-->opcode = 0)
+   parameters:
+                @param word -the pointer to the operation
+    return:
+                @return opcode number,otherwise NON
+ */
+int get_opcode(char *word);
+/*
+   This function return the number of operation if it valid operation
+   parameters:
+                @param word -the pointer to the operation
+    return:
+                @return umber of operation,otherwise NON
+ */
+int get_num_of_operation(char *word);
+
+
+#endif
