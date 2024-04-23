23fff4d7efc77fd9bfc9c21e98a29c3fa64d66fd
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-04-23 18:35:39 +0300

    McrList

--- McrList.c
+++ McrList.c
@@ -1,13 +1,16 @@
 #include "McrList.h"
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <stdlib.h>
-struct nodeMcr{
+
+struct nodeMcr {
     char *name;/* name of the node */
-    int start;/* start index of the macro */
-    int end; /* length of the macro */
-    McrList next;  /* Next link */
+    int start;/* start index of the mcr */
+    int end; /* length of the mcr */
+    McrList next;  /* Next nodeMcr */
 };
+
+
 McrList NewMcr(char *name, int start) {
     McrList new_mcr = (McrList) calloc(1, sizeof(struct nodeMcr));
     new_mcr->name = (char *) calloc(1, (strlen(name) + 1) * sizeof(char));
@@ -17,8 +20,9 @@ McrList NewMcr(char *name, int start) {
     new_mcr->next = NULL;
     return new_mcr;
 }
+
 void InsertMcrToList(McrList *head, McrList new_mcr) {
-    if ((*head)==NULL) /* if the list is empty */
+    if ((*head) == NULL) /* if the list is empty */
         *head = new_mcr;
     else {
         /*Make next of new node as head*/
@@ -31,18 +35,18 @@ void InsertMcrToList(McrList *head, McrList new_mcr) {
 McrList SearchMcr(McrList mcr, char *name) {
     McrList temp;
     temp = mcr;
-    if(mcr == NULL || name == NULL)
+    if (mcr == NULL || name == NULL)
         return NULL;
-    if(strcmp(mcr->name, name) == 0)
+    if (strcmp(mcr->name, name) == 0)
         return temp;
     return SearchMcr(mcr->next, name);
 }
 
 int HandelMcrName(char *name) {
     int i;
-    if (name != NULL && isalpha(name[0]) && strlen(name) <= MAX_LABEL_LENGTH) {
+    if (name != NULL && isalpha(name[0]) && strlen(name) <= MAX_MCR_LENGTH) {
         for (i = 0; i < strlen(name); i++) {
-            if (!isalnum(name[i]) && name[i]!='_')
+            if (!isalnum(name[i]) && name[i] != '_')
                 return FALSE;
         }
         return TRUE;
@@ -50,31 +54,31 @@ int HandelMcrName(char *name) {
     return FALSE;
 }
 
-char *getName(McrList linked) {
-    return linked->name;
+char *getNameM(McrList nodeMcr) {
+    return nodeMcr->name;
 }
 
-int getStart(McrList linked) {
-    return linked->start;
+int getStart(McrList nodeMcr) {
+    return nodeMcr->start;
 }
 
-int getEnd(McrList linked) {
-    return linked->end;
+int getEnd(McrList nodeMcr) {
+    return nodeMcr->end;
 }
 
-McrList getNext(McrList linked) {
-    return linked->next;
+McrList getNext(McrList nodeMcr) {
+    return nodeMcr->next;
 }
 
-void setStart(McrList linked, int start) {
-    linked->start = start;
+void setStart(McrList nodeMcr, int start) {
+    nodeMcr->start = start;
 }
 
-void setEnd(McrList linked, int end) {
-    linked->end = end;
+void setEnd(McrList nodeMcr, int end) {
+    nodeMcr->end = end;
 }
 
-void freelist(McrList head) {
+void freelistM(McrList head) {
     McrList prev, current = head;
     while (current != NULL) {
         prev = current;
@@ -83,3 +87,4 @@ void freelist(McrList head) {
         free(prev);
     }
 }
+
