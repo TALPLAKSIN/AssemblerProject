03bf20e701851f290dd070295ffd150c50df3027
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-04-23 18:32:25 +0300

    encodeAndWriteToFile

--- encodeAndWriteToFile.h
+++ encodeAndWriteToFile.h
@@ -1,8 +1,39 @@
-//
-// Created by טל פלקסין on 23/04/2024.
-//
+#ifndef ENCODEANDWRITETOFILE_H
+#define ENCODEANDWRITETOFILE_H
 
-#ifndef M1_ENCODEANDWRITETOFILE_H
-#define M1_ENCODEANDWRITETOFILE_H
+#include "convertAddress.h"
 
-#endif //M1_ENCODEANDWRITETOFILE_H
+/*
+   This function checks all the labels and for each label entry writes it to the file
+
+    parameters:
+                @param file_name - the name of the file
+                @param HeadTagList - Tag list pointer
+                @param entry_file - a pointer to entry file
+                @param flagsArray - array represent a flag (5 cell - ICcounter, DCcounter, EX, EN, ERROR)
+    return:
+                @return non
+*/
+void dealWithEntry(char *file_name, TagList *HeadTagList, FILE *entry_file, int *flagsArray);
+
+/*
+    This function is the main function of the second phase and the last phase in the assembler.
+    which dealing with encoding all the lines appearing in the received file.
+
+    parameters:
+                @param file_name - the name of the file
+                @param HeadTagList - Tag list pointer
+                @param flagsArray - array represent a flag (5 cell - ICcounter, DCcounter, EX, EN, ERROR)
+
+
+    return:
+                @return final object file
+*/
+int convertFileToBase4(char *file_name, TagList *HeadTagList, int *flagsArray);
+
+#endif
+
+
+
+
+
