8239af6b6331c75b3b8e6d821137229f7a0b6a71
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-04-23 18:34:09 +0300

    HandelLable

--- HandelLable.c
+++ HandelLable.c
@@ -1,55 +1,62 @@
 #include "HandelLable.h"
-int HandelTagName(char *name) {
+
+int HandelTagName(char *word) {
     int i;
-    if (name != NULL && isalpha(name[0]) && strlen(name) <= MAX_LABEL_LENGTH) {
-        for (i = 0; i < strlen(name) ;i++) {
-            if (!isalnum(name[i]))
+    if (word != NULL && isalpha(word[0]) && strlen(word) <= MAX_LABEL_LENGTH) {
+        for (i = 0; i < strlen(word); i++) {
+            if (!isalnum(word[i]))
                 return FALSE;
         }
         return TRUE;
     }
     return FALSE;
 }
-int handel_label(TagList *HeadTagList, char *labelName, int IC, int countLine, TagList *TagTemp) {
-    char restOfLine[MAX_LINE_LENGTH]={0};
-    labelName[strlen(labelName) - 1] = '\0'; /*we are cutting '\n' from it*/
-    copy(restOfLine, labelName);
-    LeftSpaces(restOfLine);
-    if (HandelTagName(restOfLine)) {
-        *TagTemp = SearchTag(*HeadTagList, restOfLine);
-        if (!(*TagTemp) &&(!savedWord(restOfLine))) {/*check that we have a legal label and exist in the symbolList*/
-            InsertTagToList(HeadTagList, *TagTemp);/*we have a legal label --> add to the symbol list*/
+
+int HandelLabel(TagList *HeadTagList, char *name_of_label, int instructionCounter, int num0fLine, TagList *TagTemp) {
+    char validLineArray[MaxInputLength] = {0};
+    name_of_label[strlen(name_of_label) - 1] = '\0'; /*we are cutting '\n' from it*/
+    make_copy(validLineArray, name_of_label);
+    removeLeftWSpaces(validLineArray);
+    removeRightWSpaces(validLineArray);
+    if (HandelTagName(validLineArray)) {
+        *TagTemp = SearchTag(*HeadTagList, validLineArray);
+        if (!(*TagTemp) &&
+            (!is_reserved_word(validLineArray))) {/*check that we have a legal label and exist in the tagList*/
+            *TagTemp = NewTag(validLineArray, instructionCounter);
+            InsertTagToList(HeadTagList, *TagTemp);/*we have a legal label --> add to the tag list*/
             return TRUE;
-    }
+        }
         if ((*TagTemp) != NULL)
-            findError("Found multiple definition of the same label", countLine);
+            identifyError("Found multiple definition of the same label", num0fLine);
         else
-            findError("label can't be a reserved word", countLine);
+            identifyError("label can't be a reserved word", num0fLine);
         return FALSE;
     }
-    if (restOfLine[0] != '\0')
-        printf("Error occurred at line %d: Invalid label name: %s\n", countLine, restOfLine);
+    if (validLineArray[0] != '\0')
+        printf("Error occurred at line %d: Invalid label name: %s\n", num0fLine, validLineArray);
     else
-        findError("the label cannot be empty", countLine);
+        identifyError("the label cannot be empty", num0fLine);
     return FALSE;
 }
 
-int handel_label_not_found(FILE *validLabelFile, TagList *HeadTagList) {
+
+int handel_label_not_found(FILE *fileValidLabels, TagList *HeadTagList) {
     int flag = TRUE;
-    char lineCheck[MAX_LINE_LENGTH] = {0};
+    char lineCheck[MaxInputLength] = {0};
     TagList TagTemp = NULL;
     char *currentLabel = NULL;
-    fseek(validLabelFile, 0, SEEK_SET);
-    while (!feof(validLabelFile)) {
-        memset(lineCheck, 0, MAX_LINE_LENGTH);
-        /* iterating through each line(in every line exist 1 label) of the checksLabelFile file */
-        fgets(lineCheck, MAX_LINE_LENGTH, validLabelFile);
+    fseek(fileValidLabels, 0, SEEK_SET);
+    while (!feof(fileValidLabels)) {
+        memset(lineCheck, 0, MaxInputLength);
+        /* iterating through each line(in every line exist 1 label) of the fileValidLabels file */
+        fgets(lineCheck, MaxInputLength, fileValidLabels);
         currentLabel = strtok(lineCheck, " \t\n\v\f\r");
         if (!currentLabel) {
-            printf("\n");
+            if (!flag)
+                printf("\n");
             return flag;
         }
-        TagTemp = SearchTag(*HeadTagList, currentLabel);/*checks if the label is defined in the symbol list */
+        TagTemp = SearchTag(*HeadTagList, currentLabel);/*checks if the label is defined in the tag list */
         if (TagTemp != NULL) {
             setUsed(TagTemp, TRUE);
             continue;
@@ -64,21 +71,23 @@ int handel_label_not_found(FILE *validLabelFile, TagList *HeadTagList) {
     return TRUE;
 }
 
-int handel_label_entry(FILE *checksEntry, TagList *HeadTagList){
-    char lineCheck[MAX_LINE_LENGTH] = {0};
+
+int handel_label_entry(FILE *ValidEntryFile, TagList *HeadTagList) {
+    char lineCheck[MaxInputLength] = {0};
     TagList TagTemp = NULL;
     char *currentLabel;
-    fseek(checksEntry, 0, SEEK_SET);
-    while (!feof(checksEntry)) {
+    fseek(ValidEntryFile, 0, SEEK_SET);
+    while (!feof(ValidEntryFile)) {
         currentLabel = NULL;
-        memset(lineCheck, 0, MAX_LINE_LENGTH);
-        /* iterating through each line(in every line exist 1 label) of the checksEntry file */
-        fgets(lineCheck, MAX_LINE_LENGTH, checksEntry);
+        memset(lineCheck, 0, MaxInputLength);
+        /* iterating through each line(in every line exist 1 label) of the ValidEntryFile file */
+        fgets(lineCheck, MaxInputLength, ValidEntryFile);
         currentLabel = strtok(lineCheck, " \t\n\v\f\r");
         if (!currentLabel)/* There are empty line */
             return TRUE;
         TagTemp = SearchTag(*HeadTagList, currentLabel);/*checks if the label is defined in the tag list */
-        if ((TagTemp) != NULL && (getType(TagTemp) != EXTERNAL)) {/*make sure that the label is not  defined as EXTERNAL type*/
+        if ((TagTemp) != NULL &&
+            (getType(TagTemp) != EXTERNAL)) {/*make sure that the label is not  defined as EXTERNAL type*/
             setType(TagTemp, ENTRY);/* if it's a legal label, type defined as ENTRY */
             continue;
         }
@@ -89,5 +98,6 @@ int handel_label_entry(FILE *checksEntry, TagList *HeadTagList){
                 printf("a label can be entry or external, but prohibited both\n");
             return FALSE;
         }
-    } return TRUE;
+    }
+    return TRUE;
 }
