dc4c1f4bb8dacfe76f12aed2b8569c512e19364a
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-04-25 20:45:03 +0300

    ValidFile

--- ValidFile.c
+++ ValidFile.c
@@ -1,99 +1,112 @@
 #include "ValidFile.h"
 
-TagList HandelAndAddTag(char *file_name,int *flagsArray) {/* each cell of the flagsArray array represent a flag (5 cell - ICcounter, DCcounter, EX, EN, ERROR ) */
-    FILE *inputFile;/*received file*/
-    FILE *outputFile;
-    FILE *fileValidLabels;/*check file for the labels that appears in received */
-    FILE *ValidEntryFile;/*check file for the  entry labels that appears in received */
-    TagList HeadTagList = NULL;
-    TagList TagTemp = NULL;
+TagList HandelAndAddTag(char *file_name,int *flagsArray) {
+    FILE *inputFile=NULL;/*Input file pointer*/
+    FILE *outputFile=NULL;/*Output file pointer*/
+    FILE *fileValidLabels=NULL;/*File for all labels that appear in te code */
+    FILE *ValidEntryFile=NULL;/*File for all entry labels that appear in te code */
+    TagList HeadTagList = NULL;/*Head of the tag list*/
+    TagList TagTemp = NULL;/*Temporary tag pointer*/
     char lineValidation[MaxInputLength] = {0};
     char originalLine[MaxInputLength] = {0};
-    char *current_word = NULL; /* a pointer to current word */
-    int num0fLine = 0;/*count line of the current line */
+    char *current_word = NULL; /*  Current word pointer */
+    int num0fLine = 0;/*Line number counter */
     int instructionCounter = 100; /*instruction counter*/
     int dataCounter = 0;/*data counter*/
-    long prevLocation;
-
+    long prevLocation=0;/*Previous location in file*/
+    char c3='\0';
+    /*Open necessary files*/
     inputFile = openTheFile(file_name, "am", "r");
     outputFile = openTheFile("tempFile", "am", "w+");
-    fileValidLabels = openTheFile("LabelFile", "am",
-                                  "w+");/*open validFile to write all labels that declare in received file */
-    ValidEntryFile = openTheFile("EntryFile", "am",
-                                 "w+");/*open Entry File to write all labels with .entry declaration */
-    if (!inputFile) {/* If the file couldn't be opened for reading */
-        identifyError("Couldn't open input file!", num0fLine);
-        flagsArray[ERROR] = EXIST;
+    fileValidLabels = openTheFile("LabelFile", "am","w+");
+    ValidEntryFile = openTheFile("EntryFile", "am","w+");
+    /*Check if input file opened successfully*/
+    if (!inputFile) {
+        identifyError("Failed to open the input file.", num0fLine);
+        flagsArray[ERROR] = OCCUR;
         return NULL;
     }
     printf("Beginning primary iteration on the file: %s...\n", file_name);
-    while (!feof(inputFile)) { /*Go line by line in the received file*/
+    while (!feof(inputFile)) { /*Iterating through the input file*/
         TagTemp = NULL;
         prevLocation = ftell(inputFile);
         readAndCopyRow(inputFile, lineValidation, originalLine);
         num0fLine++;
-        current_word = strtok(lineValidation, " \t\n\v\f\r");/* cut the first word in the line */
-        if (!current_word || current_word[0] == ';')/*A comment or a blank line*/
+        /* a pointer to the current word */
+        current_word = strtok(lineValidation, " \t\n\v\f\r");
+        /*Skip empty lines or comments */
+        if (!current_word || current_word[0] == ';')
             continue;/*continue to the next line*/
-        if (current_word[strlen(current_word) - 1] == ':') {/*Checking whether a label*/
+        if (current_word[strlen(current_word) - 1] == ':') {/* Check for label declaration */
+            /* Handle label*/
             if (!HandelLabel(&HeadTagList, current_word, instructionCounter, num0fLine,
-                             &TagTemp)) {/*checks validate of the label*/
-                flagsArray[ERROR] = EXIST;/* if a occurred an error */
+                             &TagTemp)) {
+                flagsArray[ERROR] = OCCUR;/* In case of an error*/
                 continue;
             } else {
-                current_word = strtok(NULL, " \t\n\v\f\r");/* cut the next word in the line */
+                current_word = strtok(NULL, " \t\n\v\f\r");
                 if (!current_word) {
-                    identifyError("Found an empty label declaration", num0fLine);
-                    flagsArray[ERROR] = EXIST;/* if  occurred an error */
+                    identifyError("A label declaration without any content", num0fLine);
+                    flagsArray[ERROR] = OCCUR;
+                    continue;
+                }
+                else if (!strcmp(".define", current_word)) {
+                    identifyError("You must not define a label on a line that is a DEFAIN definition statement", num0fLine);
+                    flagsArray[ERROR] = OCCUR;
                     continue;
                 }
             }
         }
-        if (current_word[0] == '.') { /*Checking whether is instruction*/
+        if (current_word[0] == '.') { /*Verifying if it is an instruction */
+            /* Handle instruction*/
             if (!instructionsFormat(&HeadTagList, current_word, &dataCounter, ValidEntryFile, num0fLine, &TagTemp,
                                     flagsArray, prevLocation, inputFile, originalLine)) {
-                flagsArray[ERROR] = EXIST;/* if a occurred an error */
+                flagsArray[ERROR] = OCCUR;/* In case of an error*/
             }
-            fprintf(outputFile, "%s", originalLine);/*Copy the row without change*/
+            fprintf(outputFile, "%s", originalLine);/*Copy the original line*/
             continue;
-        } /* if the current word is a command */
-        if (check_op_name(current_word)) {
+        }
+        if (check_op_name(current_word)) {/*Verifying if it is a command */
             if (!validateCommand(current_word, &instructionCounter, fileValidLabels, num0fLine, &HeadTagList,
                                  originalLine)) {
-                flagsArray[ERROR] = EXIST;/* if a occurred an error */
+                flagsArray[ERROR] = OCCUR;/* In case of an error*/
                 continue;
             }
         } else {
             printf("Error occurred at line %d: Invalid command: '%s'\n", num0fLine, current_word);
-            flagsArray[ERROR] = EXIST;/* if a occurred an error */
+            flagsArray[ERROR] = OCCUR;/* if a occurred an error */
         }
-        fprintf(outputFile, "%s", originalLine);/*Copy the row without change*/
+        fprintf(outputFile, "%s", originalLine);/*Copy the original line*/
     }
     deleteFile(file_name, "am");
     inputFile = openTheFile(file_name, "am", "w");
     fseek(outputFile, 0, SEEK_SET);
-    while (!feof(outputFile))
-        fputc(fgetc(outputFile), inputFile);
+    while (!feof(outputFile)) {
+        c3=(char)fgetc(outputFile);
+        if(c3!=EOF)
+            fputc(c3, inputFile);
+    }
     deleteFile("tempFile", "am");
-    changeICTags(&HeadTagList,
-                 instructionCounter); /* Updating all data and string tag (add IC final value to their address counter) */
+    changeICTags(&HeadTagList,instructionCounter); /* Update instruction counter tags */
     if (!handel_label_entry(ValidEntryFile, &HeadTagList))
-        flagsArray[ERROR] = EXIST;/* exist .entry label that not valid in the file*/
+        flagsArray[ERROR] = OCCUR;/* In the file, there is an 'entry' label that does not valid.*/
     if (!handel_label_not_found(fileValidLabels, &HeadTagList))
-        flagsArray[ERROR] = EXIST;/* exist label that not defined in the file*/
-    flagsArray[ICcounter] = instructionCounter;/*save final IC to be use in the second phase*/
-    flagsArray[DCcounter] = dataCounter;/*save final DC to be use in the second phase*/
+        flagsArray[ERROR] = OCCUR;/*There is a label present in the file that has not been defined*/
+    /* Update final instructionCounter and dataCounter for phase 2*/
+    flagsArray[ICcounter] = instructionCounter;
+    flagsArray[DCcounter] = dataCounter;
     closeTheFiles(inputFile, fileValidLabels, ValidEntryFile);/* free all files*/
     if (HeadTagList != NULL)
-        allUsed(HeadTagList, 1);/*this method checks for unused tags*/
-    if (flagsArray[ERROR] == EXIST) {/* if a error occurred we stop the process and free/delete all memory we used*/
-        printf("First iteration on file: %s.am failed!\n", file_name);
+        allUsed(HeadTagList, 1);/*unused tags*/
+    if (flagsArray[ERROR] == OCCUR) {/*  In case of an error - stop the process and free/delete all memory*/
+        printf("phase one on the received file: %s.am has failed\n", file_name);
         deleteFile(file_name, "am");
-        freelist(HeadTagList);/* free the linked-lists */
+        freelist(HeadTagList);/* free the tag list */
         return NULL;
     }
+    /* Print success message */
     printf("Complete primary iteration on file: %s.am!\n", file_name);
-    print1(HeadTagList);
     return HeadTagList;
 }
+
 
