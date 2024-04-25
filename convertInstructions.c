b91d54e826962a1abdc773ec8a43f7edcda1ddf8
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-04-25 20:31:37 +0300

    convertInstructions

--- convertInstructions.c
+++ convertInstructions.c
@@ -1,35 +1,36 @@
 #include "convertInstructions.h"
 
 void dealWithInstructions(TagList *HeadTagList, char *action, int *dataCounter, FILE *data_file) {
-    if (!strcmp(".data", action))
+    if (!strcmp(".data", action))/*Checking if this it is a data order and handling accordingly*/
         convertDataToBaseFour(strtok(NULL, ""), dataCounter, data_file);
-    if (!strcmp(".string", action))
+    if (!strcmp(".string", action))/*Checking if this it is a string order and handling accordingly*/
         convertStringToBaseFour(strtok(NULL, ""), dataCounter, data_file);
 }
 
 void convertDataToBaseFour(char *currLine, int *dataCounter, FILE *data_file) {
     char *nextNumber = NULL;
     char validLineArray[MaxInputLength] = {0};
+    /*save the real line without changing it*/
     make_copy(validLineArray, currLine);
-    nextNumber = strtok(validLineArray, " \t\n\v\f\r,");/* cut the first number from the string */
-    while (nextNumber != NULL) {
-        convertToBaseFour(nextNumber, FALSE, data_file, dataCounter);/*encoding the input and write to the data_file*/
-        nextNumber = strtok(NULL, " \t\n\v\f\r,");/* cut the next number from the string */
+    /*take the first number*/
+    nextNumber = strtok(validLineArray, " \t\n\v\f\r,");
+    while (nextNumber != NULL) {/*throw all the number in data instruction*/
+        convertToBaseFour(nextNumber, FALSE, data_file, dataCounter);/*write the number to the data_file*/
+        nextNumber = strtok(NULL, " \t\n\v\f\r,");/*take the next number*/
     }
 }
 
 void convertStringToBaseFour(char *currLine, int *dataCounter, FILE *data_file) {
-    int i;
+    int i=0;
     char validLineArray[MaxInputLength] = {0};
+    /*save the real line without changing it*/
     make_copy(validLineArray, currLine);
-    for (i = 1; validLineArray[i] != '\"'; i++) {/*over the string char by char*/
-        convertToBaseFour(&validLineArray[i], TRUE, data_file,
-                          dataCounter);/*encoding the input and write to the data_file*/
-    }
-    *dataCounter = *dataCounter + 1;
-    if (*dataCounter < 1000)
+    for (i = 1; validLineArray[i] != '\"'; i++)/*throw all the char in string instruction*/
+        convertToBaseFour(&validLineArray[i], TRUE, data_file,dataCounter);/*write the char to the data_file*/
+    *dataCounter = *dataCounter + 1;/*Adds one to the data counter*/
+    if (*dataCounter < 1000)/*complete '0' to the file if necessary*/
         fprintf(data_file, "0");
-    fprintf(data_file, "%d\t*******\n", *dataCounter);/*encoding 0 line in the end of the string*/
+    fprintf(data_file, "%d\t*******\n", *dataCounter);/*write 0 line to the data_file*/
 
 }
 
