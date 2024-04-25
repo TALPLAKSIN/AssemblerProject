ab0e5839a67093d084b685a5e18ae5c9a47e2700
TALPLAKSIN <156201744+TALPLAKSIN@users.noreply.github.com>
2024-04-25 20:30:46 +0300

    convertAddress

--- convertAddress.c
+++ convertAddress.c
@@ -1,8 +1,7 @@
 #include "convertAddress.h"
 
-void dealWithAddress(addressingMode operand_mode, TagList *HeadTagList, char *parameter, int *instructionCounter,
-                     FILE *instructions_file, FILE *extern_file, int num1_Or_num2) {
-    *instructionCounter = *instructionCounter + 1;
+void dealWithAddress(addressingMode operand_mode, TagList *HeadTagList, char *parameter, int *instructionCounter,FILE *instructions_file, FILE *extern_file, int num1_Or_num2) {
+    *instructionCounter = *instructionCounter + 1;/*Adds one to the instruction counter*/
     /*encoding the extra lines according to the appropriate method*/
     if (operand_mode == constantIndexAddress)
         convertConstIndexToBase4(parameter, instructionCounter, instructions_file, HeadTagList, extern_file);
@@ -19,26 +18,24 @@ void dealWithAddress(addressingMode operand_mode, TagList *HeadTagList, char *pa
 }
 
 
-void convertSentenceToBase4(TagList *HeadTagList, char *sentence, int *instructionCounter, FILE *instructions_file,
-                            FILE *extern_file) {
-
-    int opNumber;
+void convertSentenceToBase4(TagList *HeadTagList, char *sentence, int *instructionCounter, FILE *instructions_file,FILE *extern_file) {
+    int opNumber=0;
     opNumber = get_num_of_operation(sentence);
-    if (opNumber == 0)/*none operands command*/
+    if (opNumber == 0)/*A command with no operands at all*/
         convertFirstWordToBaseFour(get_opcode(sentence), 0, 0, 0, instructions_file, instructionCounter);
-    if (opNumber == 1)/*one operand command*/
+    if (opNumber == 1)/*A command with one operand*/
         convertOneOperand(HeadTagList, sentence, instructionCounter, instructions_file, extern_file);
-    if (opNumber == 2)/*two operand command*/
+    if (opNumber == 2)/*A command with two operands*/
         convertTwoOperand(HeadTagList, sentence, instructionCounter, instructions_file, extern_file);
 }
 
 void convertOneOperand(TagList *HeadTagList, char *sentence, int *instructionCounter, FILE *instructions_file,
                        FILE *extern_file) {
-    int opcode = 0;/*the opcode of the command*/
-    addressingMode destination_operand = 0;/*the destination address of the command*/
+    int opcode = 0;/*Represents the amount of operands of the command*/
+    addressingMode destination_operand = NOT_OCCUR;/*Represents the destination address of the command*/
     char *num_second_operand = NULL;
     opcode = get_opcode(sentence);
-    num_second_operand = strtok(NULL, " \t\n\v\f\r,");/* cut the second parameter from the line */
+    num_second_operand = strtok(NULL, " \t\n\v\f\r,");/*save the second operand*/
     destination_operand = getAddressingMode(num_second_operand, NULL, FALSE, 0, NULL);
     /*encoding the first word*/
     convertFirstWordToBaseFour(opcode, 0, destination_operand, 0, instructions_file, instructionCounter);
@@ -49,29 +46,31 @@ void convertOneOperand(TagList *HeadTagList, char *sentence, int *instructionCou
 
 void convertTwoOperand(TagList *HeadTagList, char *sentence, int *instructionCounter, FILE *instructions_file,
                        FILE *extern_file) {
-    int opcode = 0;/*the opcode of the command*/
-    addressingMode source_operand = 0;/*the source address of the command*/
-    addressingMode destination_operand = 0;/*the destination address of the command*/
+    int opcode = 0;/*Represents the amount of operands of the command*/
+    addressingMode source_operand = NOT_OCCUR;/*Represents the source address of the command*/
+    addressingMode destination_operand = NOT_OCCUR;/*Represents the destination address of the command*/
     char num_one_operand[MaxInputLength] = {0};
     char num_second_operand[MaxInputLength] = {0};
     char *temp = NULL;
     char *temp2 = NULL;
     opcode = get_opcode(sentence);
-    temp = strtok(NULL, " \t\n\v\f\r,");/*cut the first parameter from the line*/
-    temp2 = strtok(NULL, " ");
-    make_copy(num_one_operand, temp);
-    make_copy(num_second_operand, temp2);
+    temp = strtok(NULL, " \t\n\v\f\r,");/*save the first operand*/
+    temp2 = strtok(NULL, " ");/*save the second operand*/
+    make_copy(num_one_operand, temp);/*save the real one operand without changing it*/
+    make_copy(num_second_operand, temp2);/*save the real second operand without changing it*/
+    /*Deletes white characters from both ends of the string*/
     removeLeftWSpaces(num_one_operand);
     removeRightWSpaces(num_one_operand);
+    /*Deletes white characters from both ends of the string*/
     removeLeftWSpaces(num_second_operand);
     removeRightWSpaces(num_second_operand);
+    /*Mark's value appear as true or false corresponding to the need to write the label in the file for further tests*/
     source_operand = getAddressingMode(num_one_operand, NULL, FALSE, 0, NULL);
     destination_operand = getAddressingMode(num_second_operand, NULL, FALSE, 0, NULL);
     /*encoding the first word*/
     convertFirstWordToBaseFour(opcode, source_operand, destination_operand, 0, instructions_file, instructionCounter);
     /*encoding the extra line*/
-    if (destination_operand == directRegisterAddress &&
-        source_operand == directRegisterAddress) {/*increase IC counter for 2*/
+    if (destination_operand == directRegisterAddress && source_operand == directRegisterAddress) {
         *instructionCounter = *instructionCounter + 1; /*encoding them as a one line*/
         convertOperandToBaseFour((get_reg_number(num_one_operand) << 3) | (get_reg_number(num_second_operand)), 0,
                                  instructions_file, instructionCounter);
@@ -84,9 +83,9 @@ void convertTwoOperand(TagList *HeadTagList, char *sentence, int *instructionCou
 }
 
 void convertImmediateToBase4(char *parameter, int *instructionCounter, FILE *instructions_file) {
-    char *temp;
+    char *temp=NULL;
     char num_one_operand[MaxInputLength] = {0};
-    int parameter_to_int;
+    int parameter_to_int=0;
     /*Conversion corresponds to base 4 according to the current addressing method*/
     make_copy(num_one_operand, parameter);
     parameter = strtok(num_one_operand, "#");
@@ -97,8 +96,8 @@ void convertImmediateToBase4(char *parameter, int *instructionCounter, FILE *ins
 void convertConstIndexToBase4(char *parameter, int *instructionCounter, FILE *instructions_file, TagList *HeadTagList,
                               FILE *extern_file) {
     char validLineArray[MaxInputLength] = {0};
-    char *end;
-    char *temp;
+    char *end=NULL;
+    char *temp=NULL;
     int parameter_to_int = 0;
     /*Conversion corresponds to base 4 according to the current addressing method*/
     make_copy(validLineArray, parameter);
@@ -106,7 +105,7 @@ void convertConstIndexToBase4(char *parameter, int *instructionCounter, FILE *in
     end = strtok(NULL, "[] ");
     parameter_to_int = strtol(end, &temp, 10);
     convertDirectToBase4(HeadTagList, parameter, instructionCounter, instructions_file, extern_file);
-    *instructionCounter = *instructionCounter + 1;
+    *instructionCounter = *instructionCounter + 1;/*Adds one to the instruction counter*/
     convertOperandToBaseFour((int) parameter_to_int, 0, instructions_file, instructionCounter);
 }
 
@@ -115,7 +114,7 @@ void convertDirectToBase4(TagList *HeadTagList, char *parameter, int *instructio
     int BIT_A_R_E = 2;
     TagList temp = NULL;
     temp = SearchTag(*HeadTagList, parameter);/* find the tag a like the label*/
-    if (getType(temp) == EXTERNAL) { /*if it is a EXTERNAL type, we write it to the extern_file file */
+    if (getType(temp) == EXTERNAL) { /*EXTERNAL - write to the extern_file*/
         fprintf(extern_file, "%s\t", getName(temp));
         if (*instructionCounter < 1000)
             fprintf(extern_file, "0");
